1807dbd7a4268b68769c841c812681195acfc419
EmileBlngr <emile.boulanger2@gmail.com>
2023-12-06 20:05:27 +0100

    Encrypt and Decrypt fully working (Console Version)

---
+++ Encrypt.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Cryptosoft
+{
+    public class Encrypt
+    {
+        public string EncryptingKey { get; set; }
+        public string SourceEncryptPath { get; set; }
+        public string TargetEncryptPath { get; set; }
+
+        public string TargetDecryptPath { get; set; }
+
+        public Encrypt() 
+        {
+            GenerateEncryptingkey();
+        }
+        public void GenerateEncryptingkey()
+        {
+            Random random = new Random();
+            EncryptingKey = "";
+            for (int i = 0; i < 128; i++)
+            {
+                EncryptingKey += random.Next(2);
+            }
+        }
+        public void EncryptData()
+        {
+            if (File.Exists(SourceEncryptPath))
+            {
+                EncryptFile(SourceEncryptPath);
+                Console.WriteLine("Encryption completed successfully.");
+            }
+            else if (Directory.Exists(SourceEncryptPath))
+            {
+                EncryptDirectory();
+            }
+        }
+        public void EncryptDirectory()
+        {
+            string[] files = Directory.GetFiles(SourceEncryptPath);
+
+            foreach (string filePath in files)
+            {
+                EncryptFile(filePath);
+            }
+
+            Console.WriteLine("Encryption completed successfully for all files in the directory.");
+        }
+        public void EncryptFile(string filePath)
+        {
+            byte[] fileContent = File.ReadAllBytes(filePath);
+
+            byte[] encryptedContent = XOREncrypt(fileContent);
+
+            string fileName = Path.GetFileName(filePath);
+            string encryptedFilePath = Path.Combine(TargetEncryptPath, fileName);
+            File.WriteAllBytes(encryptedFilePath, encryptedContent);
+        }
+
+        public byte[] XOREncrypt(byte[] data)
+        {
+            byte[] encryptedData = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                encryptedData[i] = (byte)(data[i] ^ EncryptingKey[i % EncryptingKey.Length]);
+                // ^ = XOR
+                //i % EncryptingKey.Length to ensure not to reach the end of the key
+            }
+            return encryptedData;
+        }
+        public void DecryptFiles()
+        {
+            string[] encryptedFiles = Directory.GetFiles(TargetDecryptPath);
+
+            foreach (string encryptedFilePath in encryptedFiles)
+            {
+                byte[] encryptedContent = File.ReadAllBytes(encryptedFilePath);
+
+                byte[] decryptedContent = Decrypt(encryptedContent);
+
+                string decryptedFileName = Path.GetFileName(encryptedFilePath);
+                string decryptedFilePath = Path.Combine(TargetDecryptPath, decryptedFileName);
+
+                File.WriteAllBytes(decryptedFilePath, decryptedContent);
+
+                Console.WriteLine($"Decryption completed. File saved at: {decryptedFilePath}");
+            }
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            byte[] decryptedData = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                decryptedData[i] = (byte)(data[i] ^ EncryptingKey[i % EncryptingKey.Length]);
+            }
+            return decryptedData;
+        }
+        public string ConvertToLocalUNC(string localPath)
+        {
+            try
+            {
+                string uncPath = Path.GetFullPath(localPath);
+                return uncPath;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error converting path to UNC: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cryptosoft;
+
+Encrypt encrypt = new Encrypt();
+bool run = true;
+Console.WriteLine("Welcome to Cryptosoft !");
+while (run)
+{
+    try
+    {
+        Console.WriteLine("Choose an option :");
+        Console.WriteLine("1. Encrypt files");
+        Console.WriteLine("2. Decrypt files");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine("Encrypting files...");
+                EncryptView();
+                break;
+            case "2":
+                Console.WriteLine("Decrypting files...");
+                DecryptView();
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please enter 1 or 2.");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error : {ex.Message}");
+    }
+}
+
+void EncryptView()
+{
+    Console.WriteLine("Choose the path of the file or folder you want to encrypt :");
+    encrypt.SourceEncryptPath = Console.ReadLine();
+    encrypt.SourceEncryptPath = encrypt.ConvertToLocalUNC(encrypt.SourceEncryptPath);
+    Console.WriteLine("Choose the path of the folder where you want to have the result :");
+    encrypt.TargetEncryptPath = Console.ReadLine();
+    encrypt.TargetEncryptPath = encrypt.ConvertToLocalUNC(encrypt.TargetEncryptPath);
+    if (File.Exists(encrypt.SourceEncryptPath) || Directory.Exists(encrypt.SourceEncryptPath))
+    {
+        encrypt.EncryptData();
+    }
+    else
+    {
+        Console.WriteLine("Le chemin spécifié n'existe pas.");
+    }
+}
+void DecryptView()
+{
+    Console.WriteLine("Choose the path of the file or folder you want to decrypt :");
+    encrypt.TargetDecryptPath = Console.ReadLine();
+    encrypt.TargetDecryptPath = encrypt.ConvertToLocalUNC(encrypt.TargetDecryptPath);
+    if (Directory.Exists(encrypt.TargetDecryptPath))
+    {
+        encrypt.DecryptFiles();
+    }
+    else
+    {
+        Console.WriteLine("Le chemin spécifié n'existe pas.");
+    }
+}
